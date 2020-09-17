2d35700fae175ac3737834a694a3fb2a84904e52
Inura-phi <mushikaito@gmail.com>
2020-09-18 05:54:59 +0900

    damedatta

--- XSS_tool/persistent_xss_vulnerable.py
+++ XSS_tool/persistent_xss_vulnerable.py
@@ -46,9 +46,9 @@ def get_tasklist():
     body = '<table border="1">'
     for row in result:
         body += '<tr><td>'
-        body += row[0].encode(encoding="utf-8", errors="strict")
+        body += str(row[0], 'utf-8')
         body += '</td><td>'
-        body += row[1].encode(encoding="utf-8", errors="strict")
+        body += str(row[1], 'utf-8')
         body += '</td><tr>'
 
     body += '</table>'
